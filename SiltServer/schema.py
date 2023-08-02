2c10ab9c43e428e4255e8a53bae289a5d3074dbc
David Byrne <david.byrne222@gmail.com>
2023-08-02 17:19:03 +0100

    seperated song models to input and output to remove redunadant fields in input

--- SiltServer/schema.py
+++ SiltServer/schema.py
@@ -3,8 +3,7 @@ from datetime import date
 from pydantic import BaseModel
 
 
-class Song(BaseModel):
-    id: int
+class SongIn(BaseModel):
     song: str
     album: str
     artist: str
@@ -12,6 +11,13 @@ class Song(BaseModel):
     spotify_link: str = None
     youtube_link: str = None
     itunes_link: str = None
+
+    class Config:
+        orm_mode = True
+
+
+class SongOut(SongIn):
+    id: int
     added_date: date
 
     class Config:
