f8daea2c5c35af3bcc5baa5ca799a7813884b409
David Byrne <david.byrne222@gmail.com>
2023-08-03 19:08:45 +0100

    extracted 401 raise to own method for DRY

--- SiltServer/auth/dependencies.py
+++ SiltServer/auth/dependencies.py
@@ -14,19 +14,25 @@ def get_user(username: bytes) -> Optional[UserModel]:
     return db.session.query(UserModel).filter(UserModel.username == username.decode()).first()
 
 
+def _raise_401():
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Incorrect email and/or password",
+        headers={"WWW-Authenticate": "Basic"},
+    )
+
+
 def authorise(credentials: Annotated[HTTPBasicCredentials, Depends(security)]) -> dict:
     current_username_bytes = credentials.username.encode("utf8")
     current_password_bytes = credentials.password.encode("utf8")
 
+    # Check user exists in the db
     user = get_user(current_username_bytes)
 
     if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect email and/or password",
-            headers={"WWW-Authenticate": "Basic"},
-        )
+        _raise_401()
 
+    # check username
     is_correct_username = secrets.compare_digest(
         current_username_bytes, user.username.encode()
     )
@@ -38,10 +44,6 @@ def authorise(credentials: Annotated[HTTPBasicCredentials, Depends(security)]) -
 
     # check both are correct
     if not (is_correct_username and is_correct_password):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect email and/or password",
-            headers={"WWW-Authenticate": "Basic"},
-        )
+        _raise_401()
 
     return {"login": "success"}
