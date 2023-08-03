8bf59737041bcb16d1c2ceb1f66760ece0c48fdd
David Byrne <david.byrne222@gmail.com>
2023-08-03 16:48:54 +0100

    refactored to resource dir structure

--- SiltServer/main.py
+++ SiltServer/main.py
@@ -2,11 +2,10 @@ import os
 
 import uvicorn
 from fastapi import FastAPI
-from fastapi_sqlalchemy import DBSessionMiddleware, db
+from fastapi_sqlalchemy import DBSessionMiddleware
 from pydantic_settings import BaseSettings
 
-from models import Song as ModelSong
-from schema import SongOut as SchemaSongOut, SongIn as SchemaSongIn
+from songs import router as songs_router
 
 
 class Settings(BaseSettings):
@@ -21,36 +20,9 @@ settings = Settings()
 app = FastAPI()
 app.add_middleware(DBSessionMiddleware, db_url=os.environ['DATABASE_URL'])
 
-
-# app.include_router(auth_router.router)
-# app.include_router(issues_router.router)
-
-
-@app.get("/")
-async def root():
-    songs = db.session.query(ModelSong).all()
-    return songs
-
-
-@app.post('/song/', response_model=SchemaSongOut)
-async def post_song(song: SchemaSongIn):
-    db_song = ModelSong(
-        song=song.song,
-        album=song.album,
-        artist=song.artist,
-        discog_link=song.discog_link,
-        spotify_link=song.spotify_link,
-        youtube_link=song.youtube_link,
-        itunes_link=song.itunes_link
-    )
-    db.session.add(db_song)
-    db.session.commit()
-    return db_song
-
+app.include_router(songs_router.router)
 
 if __name__ == "__main__":
-    print(settings.model_dump())
-
     uvicorn.run("main:app",
                 host=settings.SERVER_ADDR_HOST,
                 port=settings.SERVER_ADDR_PORT,
