10469f34ff70e9883497b28b80d930381218acdb
David Byrne <david.byrne222@gmail.com>
2023-08-10 16:45:17 +0100

    linted

--- SiltServer/dependencies/auth.py
+++ SiltServer/dependencies/auth.py
@@ -7,11 +7,9 @@ from jose import JWTError, jwt
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 
-
 from SiltServer.database.crud import get_user
 from SiltServer.database.database import get_db
 from SiltServer.models.auth import ModelUser
-
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -45,7 +43,6 @@ def authenticate_user(db, username: str, password: str):
 
 
 def create_access_token(user: ModelUser, expires_delta: Optional[timedelta]):
-
     # calculate token expiry time
     expire = datetime.utcnow() + (expires_delta if expires_delta else timedelta(minutes=15))
 
