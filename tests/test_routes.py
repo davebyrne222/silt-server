9e82d3b011fac6972db5afee22d11831fe2e769f
David Byrne <david.byrne222@gmail.com>
2023-09-06 18:09:40 +0100

    added tests for token endpoint

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,9 +1,19 @@
 from fastapi.testclient import TestClient
 
 from SiltServer.main import app
+from SiltServer.schemas.auth import Token
 from SiltServer.schemas.songs import PaginatedResponse
 
 client = TestClient(app)
+
+
+def assert_valid_response(model, json):
+    try:
+        model(**json)
+    except ValueError as e:
+        assert False, f"Response structure does not match the Pydantic model: {e}"
+
+    assert True
 
 
 def test_get_songs():
@@ -11,9 +21,18 @@ def test_get_songs():
     assert response.status_code == 200
 
     # Validate response against Pydantic model
-    try:
-        PaginatedResponse(**response.json())
-    except ValueError as e:
-        assert False, f"Response structure does not match the Pydantic model: {e}"
+    assert_valid_response(PaginatedResponse, response.json())
 
-    assert True
+
+def test_login_failure():
+    response = client.post("/token", data=dict(username="none", password="none"))
+    assert response.status_code == 401
+
+
+def test_login_success():
+    response = client.post("/token", data=dict(username="davebyrne", password="1234"))
+    assert response.status_code == 200
+
+    assert_valid_response(Token, response.json())
+
+
