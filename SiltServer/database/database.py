f4e0b2c9000017c8d1102fed4ddd82a3e2917adf
David Byrne <david.byrne222@gmail.com>
2023-09-25 18:14:05 +0100

    resolved deprecated import

--- SiltServer/database/database.py
+++ SiltServer/database/database.py
@@ -1,8 +1,7 @@
 import os
 
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 # DB connection / interface
 engine = create_engine(os.environ['DATABASE_URL'])
