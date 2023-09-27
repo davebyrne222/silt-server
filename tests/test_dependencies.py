06cfae33593cd094dcbd53e323080eac6ca0dee7
David Byrne <david.byrne222@gmail.com>
2023-09-27 18:28:32 +0100

    added tests for checking token validity

--- tests/test_dependencies.py
+++ tests/test_dependencies.py
@@ -6,17 +6,18 @@ from fastapi import HTTPException
 from jose import jwt
 
 from SiltServer.database.database import get_db
-from SiltServer.dependencies.auth import authenticate_user, create_access_token, _get_username_from_token
+from SiltServer.dependencies.auth import authenticate_user, create_access_token, verify_token
 from SiltServer.dependencies.exceptions import raise_401_invalid_creds, raise_401_invalid_token, raise_401_expired_token
 from SiltServer.models.auth import ModelUser
 
 # Generic user for testing auth functions
-user = ModelUser(
+USER = ModelUser(
     id=1000,
-    username="testuser",
+    username="davebyrne",
     hash="1234",
-    secret="9dded9fc10ecf1f601b605e8b56eba202de7eb5254efd33db4761b8065568a68"
+    secret="542078bc5fc349b3b499994f6c310270bc5d915c7479ad64a8857984541ce943"
 )
+
 
 def test_raise_401_invalid_creds():
     with pytest.raises(HTTPException) as exc:
@@ -46,13 +47,40 @@ def test_authenticate_user():
 
 @pytest.fixture(name="token")
 def get_jwt_token():
-    return create_access_token(user, timedelta(15))
+    return create_access_token(USER, timedelta(15))
 
 
 def test_create_access_token(token):
-    decoded = jwt.decode(token, key=user.secret)
-    assert decoded.get("sub") == user.username
+    decoded = jwt.decode(token, key=USER.secret)
+    assert decoded.get("sub") == USER.username
 
-def test_get_username_from_token(token):
-    username = _get_username_from_token(token)
-    assert username == user.username
+
+def test_verify_token_success(token):
+    db = next(get_db())
+    assert not verify_token(db, token)
+
+
+def test_verify_token_invalid_username():
+    """Test to ensure username decoded from JWT is valid i.e. a user in the database"""
+    db = next(get_db())
+
+    invalid_user = ModelUser(
+        id=USER.id,
+        username="testuser",
+        hash=USER.hash,
+        secret=USER.secret
+    )
+
+    token = create_access_token(invalid_user, timedelta(15))
+
+    with pytest.raises(HTTPException) as exc:
+        verify_token(db, token)
+
+    assert exc.value.detail == "Could not validate token"
+
+
+def test_verify_token_invalid_token():
+    db = next(get_db())
+    with pytest.raises(HTTPException) as exc:
+        verify_token(db, "faketoken")
+    assert exc.value.detail == "Could not validate token"
