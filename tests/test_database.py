35ee3de99082954f4025c046d4c3425dd55b4193
David Byrne <david.byrne222@gmail.com>
2023-09-27 18:57:49 +0100

    added test for get_user

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,8 +1,20 @@
+import pytest
 from sqlalchemy.orm.session import Session
 
+from SiltServer.database.crud import get_user
 from SiltServer.database.database import get_db
+from SiltServer.models.auth import ModelUser
 
 
-def test_get_db():
-    db = next(get_db())
+@pytest.fixture(name="db")
+def get_db_instance():
+    return next(get_db())
+
+
+def test_get_db(db):
     assert isinstance(db, Session)
+
+
+def test_get_user(db):
+    user = get_user(db, 'davebyrne')
+    assert isinstance(user, ModelUser)
