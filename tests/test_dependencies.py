5bba7ce3c418d2b6afa2f739a3dacaf9e1b814d8
David Byrne <david.byrne222@gmail.com>
2023-09-27 17:36:12 +0100

    added test for _get_username_from_token

--- tests/test_dependencies.py
+++ tests/test_dependencies.py
@@ -6,10 +6,17 @@ from fastapi import HTTPException
 from jose import jwt
 
 from SiltServer.database.database import get_db
-from SiltServer.dependencies.auth import authenticate_user, create_access_token
+from SiltServer.dependencies.auth import authenticate_user, create_access_token, _get_username_from_token
 from SiltServer.dependencies.exceptions import raise_401_invalid_creds, raise_401_invalid_token, raise_401_expired_token
 from SiltServer.models.auth import ModelUser
 
+# Generic user for testing auth functions
+user = ModelUser(
+    id=1000,
+    username="testuser",
+    hash="1234",
+    secret="9dded9fc10ecf1f601b605e8b56eba202de7eb5254efd33db4761b8065568a68"
+)
 
 def test_raise_401_invalid_creds():
     with pytest.raises(HTTPException) as exc:
@@ -37,14 +44,15 @@ def test_authenticate_user():
     assert user
 
 
-def test_create_access_token():
-    user = ModelUser(
-        id=1000,
-        username="testuser",
-        hash="1234",
-        secret="9dded9fc10ecf1f601b605e8b56eba202de7eb5254efd33db4761b8065568a68"
-    )
-    token = create_access_token(user, timedelta(15))
+@pytest.fixture(name="token")
+def get_jwt_token():
+    return create_access_token(user, timedelta(15))
+
+
+def test_create_access_token(token):
     decoded = jwt.decode(token, key=user.secret)
-    assert decoded is not None
     assert decoded.get("sub") == user.username
+
+def test_get_username_from_token(token):
+    username = _get_username_from_token(token)
+    assert username == user.username
