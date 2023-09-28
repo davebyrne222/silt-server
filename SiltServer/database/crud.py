c27fc6ffabe27e5b4d736a730d020bd7950d0fbc
David Byrne <david.byrne222@gmail.com>
2023-09-28 13:07:16 +0100

    refactored functions names for clarity and consistency

--- SiltServer/database/crud.py
+++ SiltServer/database/crud.py
@@ -7,11 +7,11 @@ from SiltServer.models.songs import ModelSong
 from SiltServer.schemas.songs import PaginatedResponse, SchemaSongIn, SchemaSongOut
 
 
-def get_user(db: Session, username: str) -> Optional[ModelUser]:
+def get_user_db(db: Session, username: str) -> Optional[ModelUser]:
     return db.query(ModelUser).filter(ModelUser.username == username).first()
 
 
-def get_songs(db: Session, limit: Optional[int] = None, offset: int = 0) -> PaginatedResponse:
+def get_songs_db(db: Session, limit: Optional[int] = None, offset: int = 0) -> PaginatedResponse:
     results = db.query(ModelSong).offset(offset).limit(limit)
     total = db.query(ModelSong.id).count()
     return PaginatedResponse(
@@ -23,7 +23,7 @@ def get_songs(db: Session, limit: Optional[int] = None, offset: int = 0) -> Pagi
     )
 
 
-def create_song(db: Session, song: SchemaSongIn) -> SchemaSongOut:
+def create_song_db(db: Session, song: SchemaSongIn) -> SchemaSongOut:
     song = ModelSong(**song.__dict__)
     db.add(song)
     db.commit()
