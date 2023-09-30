ef387d037c06102116493bf266801a31ee1c062d
David Byrne <david.byrne222@gmail.com>
2023-09-30 18:22:03 +0100

    added basic logging

--- SiltServer/main.py
+++ SiltServer/main.py
@@ -1,9 +1,14 @@
 import uvicorn
 from fastapi import FastAPI
+import logging
+import logging.config
 from pydantic_settings import BaseSettings
 
 from SiltServer.database.database import engine, Base
 from SiltServer.routers import songs as songs_router, auth as auth_router
+
+logging.config.fileConfig('logging_config.ini', disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
 
 
 class Settings(BaseSettings):
@@ -15,11 +20,12 @@ class Settings(BaseSettings):
 
 settings = Settings()
 
+logger.debug(f"Settings: {vars(settings)}")
+
 # Create tables from SQLAlchemy models
 Base.metadata.create_all(bind=engine, checkfirst=True)
 
 app = FastAPI()
-
 app.include_router(auth_router.router)
 app.include_router(songs_router.router)
 
@@ -27,5 +33,8 @@ if __name__ == "__main__":
     uvicorn.run("main:app",
                 host=settings.SERVER_ADDR_HOST,
                 port=settings.SERVER_ADDR_PORT,
-                reload=settings.SERVER_RELOAD
+                reload=settings.SERVER_RELOAD,
+                log_config="logging_config.ini",
+                log_level="debug"
                 )
+
