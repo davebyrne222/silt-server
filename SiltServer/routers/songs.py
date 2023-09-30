f5f7f13ff9e9aa11a1e9b243922c1fee4994ce3b
David Byrne <david.byrne222@gmail.com>
2023-09-30 18:22:36 +0100

    added logging

--- SiltServer/routers/songs.py
+++ SiltServer/routers/songs.py
@@ -1,3 +1,4 @@
+import logging
 from typing import Annotated
 
 from fastapi import APIRouter, Depends, Query
@@ -7,6 +8,8 @@ from SiltServer.database.crud import get_songs_db, create_song_db
 from SiltServer.database.database import get_db
 from SiltServer.dependencies.auth import verify_token
 from SiltServer.schemas.songs import SchemaSongOut, SchemaSongIn, PaginatedResponse
+
+logger = logging.getLogger(__name__)
 
 router = APIRouter(
     dependencies=[],
@@ -24,6 +27,7 @@ async def get_songs(
         db: Annotated[Session, Depends(get_db)],
         limit: int = Query(50, ge=1, le=50),
         offset: int = Query(0, ge=0)):
+    logger.debug(f"Getting songs with limit={limit}, offset={offset}")
     return get_songs_db(db, limit=limit, offset=offset)
 
 
@@ -34,4 +38,5 @@ async def get_songs(
     response_model=SchemaSongOut,
     status_code=200)
 async def post_song(song: SchemaSongIn, db: Annotated[Session, Depends(get_db)]):
+    logger.debug(f"Adding song: {vars(song)}")
     return create_song_db(db, song)
